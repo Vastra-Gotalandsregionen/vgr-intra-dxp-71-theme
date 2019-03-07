8b14efa4f98d30a135d5d415b1e35fd99cc7bf69
mrg3kko <erik.andersson@monator.com>
2019-03-07 16:15:48 +0100

    Added fix for sorting ifeed by url parameters in js

--- src/js/jq.js
+++ src/js/jq.js
@@ -83,7 +83,7 @@
           dataType: "jsonp",
           url: url,
           success: function(data) {
-            ifeedJsonpSuccessCallback(data, feedContainer, templateNode);
+            ifeedJsonpSuccessCallback(data, feedContainer, templateNode, url);
           },
           failure: function(data) {
             // TODO: write failure callback
@@ -119,7 +119,22 @@
 
   }
 
-  function ifeedJsonpSuccessCallback(json, feedContainer, templateNode) {
+  function ifeedJsonpSuccessCallback(json, feedContainer, templateNode, url) {
+
+    // FLAGGA PB
+    var urlParams = getAllUrlParams(url);
+
+    var sortParam = urlParams['by'];
+    if(sortParam == '') {
+      sortParam = 'dc.title';
+    }
+
+    var sortAsc = true;
+    if(urlParams['dir'] == 'desc') {
+      sortAsc = false;
+    }
+
+    json = sortJson(json, sortParam, sortAsc);
 
     var templateNodeContent = templateNode.html();
     var template = Handlebars.compile(templateNodeContent);
@@ -131,6 +146,77 @@
     setupIfeedDisplayTypes(feedContainer);
   }
 
+  function sortJson(json, prop, asc) {
+    var sortedJson = json.sort(function(a, b) {
+        if (asc) {
+            return (a[prop] > b[prop]) ? 1 : ((a[prop] < b[prop]) ? -1 : 0);
+        } else {
+            return (b[prop] > a[prop]) ? 1 : ((b[prop] < a[prop]) ? -1 : 0);
+        }
+    });
+    return sortedJson;
+  }
+
+  function getAllUrlParams(url) {
+
+    // get query string from url (optional) or window
+    var queryString = url.split('?')[1];
+
+    // we'll store the parameters here
+    var obj = {};
+
+    // if query string exists
+    if (queryString) {
+
+      // stuff after # is not part of query string, so get rid of it
+      queryString = queryString.split('#')[0];
+
+      // split our query string into its component parts
+      var arr = queryString.split('&');
+
+      for (var i = 0; i < arr.length; i++) {
+        // separate the keys and the values
+        var a = arr[i].split('=');
+
+        // set parameter name and value (use '' if empty)
+        var paramName = a[0];
+        var paramValue = typeof (a[1]) === 'undefined' ? '' : a[1];
+
+        // if the paramName ends with square brackets, e.g. colors[] or colors[2]
+        if (paramName.match(/\[(\d+)?\]$/)) {
+
+          // create key if it doesn't exist
+          var key = paramName.replace(/\[(\d+)?\]/, '');
+          if (!obj[key]) obj[key] = [];
+
+          // if it's an indexed array e.g. colors[2]
+          if (paramName.match(/\[\d+\]$/)) {
+            // get the index value and add the entry at the appropriate position
+            var index = /\[(\d+)\]/.exec(paramName)[1];
+            obj[key][index] = paramValue;
+          } else {
+            // otherwise add the value to the end of the array
+            obj[key].push(paramValue);
+          }
+        } else {
+          // we're dealing with a string
+          if (!obj[paramName]) {
+            // if it doesn't exist, create property
+            obj[paramName] = paramValue;
+          } else if (obj[paramName] && typeof obj[paramName] === 'string'){
+            // if property does exist and it's a string, convert it to an array
+            obj[paramName] = [obj[paramName]];
+            obj[paramName].push(paramValue);
+          } else {
+            // otherwise add the property
+            obj[paramName].push(paramValue);
+          }
+        }
+      }
+    }
+
+    return obj;
+  }
 
 
 
